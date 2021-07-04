d3ecb86bb6844b04857d4bfbbcac865cfbe3e687
tpsantos2120 <thiagocroza472@gmail.com>
2021-07-04 20:42:47 -0300

    Add User and Watchlist setters and getters

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -19,5 +19,67 @@ public class User {
     private Date createdAt = new Date();
     private ArrayList<Watchlist> watchlistId;
 
+    public ObjectId getId() {
+        return id;
+    }
 
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public ArrayList<Watchlist> getWatchlistId() {
+        return watchlistId;
+    }
+
+    public void setWatchlistId(ArrayList<Watchlist> watchlistId) {
+        this.watchlistId = watchlistId;
+    }
 }
--- src/main/java/models/Watchlist.java
+++ src/main/java/models/Watchlist.java
@@ -28,4 +28,139 @@ public class Watchlist {
     private ArrayList<String> creators;
     private ArrayList<String> added_by;
 
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getMedia_type() {
+        return media_type;
+    }
+
+    public void setMedia_type(String media_type) {
+        this.media_type = media_type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getImdb_id() {
+        return imdb_id;
+    }
+
+    public void setImdb_id(String imdb_id) {
+        this.imdb_id = imdb_id;
+    }
+
+    public String getImdb_rating() {
+        return imdb_rating;
+    }
+
+    public void setImdb_rating(String imdb_rating) {
+        this.imdb_rating = imdb_rating;
+    }
+
+    public String getVote_count() {
+        return vote_count;
+    }
+
+    public void setVote_count(String vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    public String getYoutube_trailer_key() {
+        return youtube_trailer_key;
+    }
+
+    public void setYoutube_trailer_key(String youtube_trailer_key) {
+        this.youtube_trailer_key = youtube_trailer_key;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public ArrayList<String> getStars() {
+        return stars;
+    }
+
+    public void setStars(ArrayList<String> stars) {
+        this.stars = stars;
+    }
+
+    public ArrayList<String> getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(ArrayList<String> directors) {
+        this.directors = directors;
+    }
+
+    public ArrayList<String> getCreators() {
+        return creators;
+    }
+
+    public void setCreators(ArrayList<String> creators) {
+        this.creators = creators;
+    }
+
+    public ArrayList<String> getAdded_by() {
+        return added_by;
+    }
+
+    public void setAdded_by(ArrayList<String> added_by) {
+        this.added_by = added_by;
+    }
 }
